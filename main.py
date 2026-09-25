dc26fb3de91a8713c601b9726d7a52fee44713a6
dheeraj <dheerajmishra753402@gmail.com>
2026-09-25 16:40:31 +0530

    Initial commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def water_reminder():
             timeout=10
         )
         # time.sleep(3600)  # Remind every hour
-        time.sleep(3)   # For testing purposes, remind every 3 seconds
+        # time.sleep(3)   # For testing purposes, remind every 3 seconds
 
 
 water_reminder()
